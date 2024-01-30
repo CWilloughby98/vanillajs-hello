9e753e4a9324e26cfff620361268c1b5d44a173e
CWilloughby98 <104572309+CWilloughby98@users.noreply.github.com>
2024-01-30 22:45:45 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// Añadimos un botón para obtener combinaciones de frases
+let button = document.getElementById("button");
+button.onclick = function buttonFunc() {
+  getRandomExcuse();
 };
+// button.addEventListener("click", getRandomExcuse); // PREGUNTA!!!: Es addEventListener mejor??
+
+function getRandomExcuse() {
+  // write your code here
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  // Código reutilizable que puede adaptarse a cambios en los arrays
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+  //Seleccionamos el elemento <p> excusa del HTML y lo modificamos
+  document.getElementById("excuse").innerHTML = excuse;
+
+  //console.log(excuse);
+}
+// Moví en onload abajo para que no me diera problemas con el botón
+window.onload = getRandomExcuse;
